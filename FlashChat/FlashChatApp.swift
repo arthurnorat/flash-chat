8d4a2e77116b9bf1cdea3a4d4fdf62d5657ae221
Arthur Norat <norat.arthur@gmail.com>
2025-10-08 17:36:41 -0300

    Initial commit: Clean SwiftUI project setup

---
+++ FlashChat/FlashChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashChatApp.swift
+//  FlashChat
+//
+//  Created by Arthur Norat on 08/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FlashChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
