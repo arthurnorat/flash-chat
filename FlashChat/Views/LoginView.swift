caf6ad5cdf971ba51f9d3183b71a89e5a0609f84
Arthur Norat <norat.arthur@gmail.com>
2025-10-20 15:19:18 -0300

    Create LoginView with email and password fields

---
+++ FlashChat/Views/LoginView.swift
@@ -0,0 +1,56 @@
+//
+//  LoginView.swift
+//  FlashChat
+//
+//  Created by Arthur Norat on 20/10/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+	
+	@State private var email: String = ""
+	@State private var password: String = ""
+	
+    var body: some View {
+		VStack(spacing: 20) {
+			
+			Spacer()
+			
+			TextField("Digite seu e-mail", text: $email)
+				.padding()
+				.background(Color.white)
+				.cornerRadius(25.0)
+				.shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+				.textInputAutocapitalization(.never)
+				.keyboardType(.emailAddress)
+			
+			SecureField("Digite sua senha", text: $password)
+				.padding()
+				.background(Color.white)
+				.cornerRadius(25.0)
+				.shadow(color: .black.opacity(0.1), radius: 4, x: 0, y: 2)
+				.textInputAutocapitalization(.never)
+			
+			
+			Button("Login") {
+				// TODO: - Handle login logic
+			}
+			.font(.system(size: 30))
+			.foregroundColor(Color(K.BrandColors.blue))
+			.frame(height: 48)
+			.frame(maxWidth: .infinity)
+			.background(Color(K.BrandColors.lightBlue))
+
+			Spacer()
+		}
+		.frame(maxHeight: .infinity)
+		.padding(.horizontal, 20)
+		.background(Color("BrandLightBlue"))
+		.ignoresSafeArea()
+	}
+}
+
+#Preview {
+    LoginView()
+}
